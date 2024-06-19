f22faf0d1d742bb9d40057bcc135b506ccff91ed
Berrutz <gabrigioi@hotmail.com>
2024-06-19 18:08:08 +0100

    GAN , Generator and Discriminator

---
+++ src/gan_t2i/models/Discriminator.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    
+    """ GAN Discriminator """
+def __init__(self , embedding_dim):
+        """
+        Initialize the Discriminator
+
+        Args:
+            embedding_dim (int): Dimension of the embedding coming from CLIP
+        """
+        super(Discriminator, self).__init__()
+
+        # Initialize the image and channel parameters
+        self.image_size = 64
+        self.num_channels = 3
+        self.embed_dim = embedding_dim                   # dimension of the embedding coming from CLIP 
+        self.projected_embed_dim = 128
+        self.ndf = 64
+        self.B_dim = 128
+        self.C_dim = 16
+
+        # Define the architecture of the discriminator
+        self.netD_1 = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(self.num_channels, self.ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(self.ndf, self.ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        
+        ######## Concat_embed class , useful to concatenate 2 embeddings of different size ###################
+        class Concat_embed(nn.Module):
+            def __init__(self, embed_dim, projected_embed_dim):
+                super(Concat_embed, self).__init__()
+                self.projection = nn.Sequential(
+                    nn.Linear(in_features=embed_dim, out_features=projected_embed_dim),
+                    nn.BatchNorm1d(num_features=projected_embed_dim),
+                    nn.LeakyReLU(negative_slope=0.2, inplace=True)
+                    )
+
+            def forward(self, inp, embed):
+                projected_embed = self.projection(embed)
+                replicated_embed = projected_embed.repeat(4, 4, 1, 1).permute(2,  3, 0, 1)
+                hidden_concat = torch.cat([inp, replicated_embed], 1)
+                return hidden_concat
+            
+        ###########################################
+
+        # Initialize the projector
+        self.projector = Concat_embed(self.embed_dim, self.projected_embed_dim)
+
+        # Define the architecture for the final layer
+        self.netD_2 = nn.Sequential(
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(self.ndf * 8 + self.projected_embed_dim, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )	
+        
+def forward(self, inp, embed):
+        """
+        Forward pass of the discriminator
+
+        Args:
+            inp (torch.Tensor): Input images
+            embed (torch.Tensor): Embeddings coming from CLIP
+
+        Returns:
+            tuple: Output logits and intermediate features
+        """
+        x_intermediate = self.netD_1(inp)
+        x = self.projector(x_intermediate, embed)
+        x = self.netD_2(x)
+
+        return x.view(-1, 1).squeeze(1) , x_intermediate
--- src/gan_t2i/models/GAN.py
+++ src/gan_t2i/models/GAN.py
@@ -1,1 +1,54 @@
+from gan_t2i.models.Generator import Generator
+from gan_t2i.models.Discriminator import Discriminator
+from torch.utils.data import DataLoader
 
+import torch.nn as nn
+import torch 
+import yaml
+
+class WGAN(nn.Module):
+    
+    def __init__(self, text_emb_model , embedding_size , p_emb_dim):
+        
+        super(WGAN, self).__init__()
+        
+        self.text_emb_model = text_emb_model
+        
+        # TODO : read PATH about the GAN Net
+        #with open('config.yaml', 'r') as f:
+        #    config = yaml.load(f)
+            
+        # TODO: Extract embedding size from CLIP
+        # coming from outside
+        
+
+        # TODO: Create generator and discriminator network 
+        self.generator = torch.nn.DataParallel( Generator(embedding_size , p_emb_dim ) )
+        self.discriminator = torch.nn.DataParallel( Discriminator(embedding_size , p_emb_dim ) )
+        
+        
+        # TODO: load checkpoints coming from already executed net
+        
+        
+    def train(self, dataset):
+        
+        # TODO : read info related to the net 
+        self.noise_dim = 100
+        self.batch_size = 32
+        self.num_workers = 4
+        self.lr = 0.05
+        self.beta1 = 0.5
+        self.num_epochs = 5
+        self.DITER = 5
+
+        self.l1_coef = 50
+        self.l2_coef = 100
+        
+        self.dataset = dataset
+        
+        # TODO: load the DataLoader with the dataset and the related parameters
+        self.data_loader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        
+        self.optimD = torch.optim.Adam(self.discriminator.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
+        self.optimG = torch.optim.Adam(self.generator.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
+        
