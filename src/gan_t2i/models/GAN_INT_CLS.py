186da61d3ee7dc03f646bb68c962b9a6f28d1185
Alex Valle <alexvalle75@gmail.com>
2024-06-22 00:49:28 +0200

    Updated Generator and main

--- src/gan_t2i/models/GAN_INT_CLS.py
+++ src/gan_t2i/models/GAN_INT_CLS.py
@@ -1,41 +1,92 @@
+from torchinfo import summary
 import torch.nn as nn
 import torch
 
+
 class Generator(nn.Module):
     
-    def __init__(self, emb_dim = 1024, noise_dim = 100, proj_emb_dim = 128):
+    def __init__(self, emb_dim, proj_emb_dim = 128, noise_dim = 100,  num_gen_features = 64):
+        """ 
+        # GAN-INT-CLS Generator
+        The generator network for a GAN-INT-CLS model. It produces images of size 64x64x3
+        starting from a latent space
+        ## Args
+        - emb_dim: The original size of the caption embedding
+        - proj_emb_dim: The size of the projection of the caption embedding   
+        - noise_dim: The original size of the noise vecotr z
+        - num_gen_features: The number of generator features across the ConvT2D layers     
+        """
+        super(Generator, self).__init__()
 
         self.emb_dim = emb_dim                  # The original size of the caption embedding
         self.noise_dim = noise_dim              # The original size of the noise vecotr z
         self.proj_emb_dim = proj_emb_dim        # The size of the projection of the caption embedding
-        self.gen_latend_dim = noise_dim + proj_emb_dim      # The size of the latent space for the generator
-        
+        self.latent_dim = noise_dim + proj_emb_dim      # The size of the latent space for the generator
+        self.n_gf = num_gen_features            # The number of generator features
 
 
-        super(Generator, self).__init__()
-        
-
-        self.proj_emb = nn.Sequential(
-            
+        # The projection network of the caption embedding
+        self.proj_emb_net = nn.Sequential(
+            nn.Linear(
+                in_features=self.emb_dim,
+                out_features=self.proj_emb_dim
+            ),
+            nn.BatchNorm1d(num_features=self.proj_emb_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True)
         )
 
 
+        # The generator network. Based on: https://github.com/aelnouby/Text-to-Image-Synthesis/blob/master/models/wgan_cls.py
         self.generator_net = nn.Sequential(
-        
+            nn.ConvTranspose2d(self.latent_dim, self.n_gf * 8, 4, 1, 0, bias=False),
+			nn.BatchNorm2d(self.n_gf * 8),
+			nn.ReLU(True),
+			# Current size => (n_gf * 8) x 4 x 4
+
+			nn.ConvTranspose2d(self.n_gf * 8, self.n_gf * 4, 4, 2, 1, bias=False),
+			nn.BatchNorm2d(self.n_gf * 4),
+			nn.ReLU(True),
+			# Current size => (n_gf * 4) x 8 x 8
+			
+            nn.ConvTranspose2d(self.n_gf * 4, self.n_gf * 2, 4, 2, 1, bias=False),
+			nn.BatchNorm2d(self.n_gf * 2),
+			nn.ReLU(True),
+			# Current size => (n_gf * 2) x 16 x 16
+			
+            nn.ConvTranspose2d(self.n_gf * 2,self.n_gf, 4, 2, 1, bias=False),
+			nn.BatchNorm2d(self.n_gf),
+			nn.ReLU(True),
+			# Current size => (n_gf) x 32 x 32
+			
+            nn.ConvTranspose2d(self.n_gf, 3, 4, 2, 1, bias=False),
+			nn.Tanh()
+            # Output size => 3 x 64 x 64
         )
 
 
 
+    def forward(self, emb_caption, z):
+        # Project the caption embedding 
+        emb_proj = self.proj_emb_net(emb_caption)
 
-    def forward(self, x):
-        return x
+        # Concatenate the projected caption embedding and the noise vector z
+        latent_vector = torch.cat([emb_proj, z], 1)
+
+        # Add a third and fourth dimension to the latent vector to match the dimension of the generator network
+        # ConvTranspose2D since it needs a tensor with 4 dimensions (batch_size, channels, height, width) 
+        latent_vector = latent_vector.unsqueeze(2).unsqueeze(3)
+
+        # Generate the image from the latent vector
+        return self.generator_net(latent_vector)
+
+
+
 
 
 class Discriminator(nn.Module):
     
     def __init__(self):
         super(Discriminator, self).__init__()
-        print("Creating GAN INT CLS Discriminator")
 
     def forward(self, x):
         return x
@@ -43,17 +94,42 @@ class Discriminator(nn.Module):
 
 class GAN_INT_CLS(nn.Module):
     
-    def __init__(self):
+    def __init__(self, emb_network, emb_dim, proj_emb_dim = 128, noise_dim = 100, num_gen_features = 64):
+        """ 
+        # The Gan-INT-CLS model 
+        The GAN-INT-CLS model. It produces images of size 64x64x3 starting from a caption
+        ## Args
+        - emb_network: The embedding network used to produce caption embeddings
+        - embedding_size: The size of the caption embedding (size of the emb_network output)
+        - proj_emb_dim: The size of the projection of the caption embedding
+        - noise_dim: The original size of the noise vecotr z
+        - num_gen_features: The number of generator features across the ConvT2D layers
+        """
         super(GAN_INT_CLS, self).__init__()
-                
-        self.generator = Generator()
+
+        self.emb_dim = emb_dim
+        self.proj_emb_dim = proj_emb_dim
+        self.noise_dim = noise_dim
+        self.num_gen_features = num_gen_features
+
+
+        # The embedding network
+        self.emb_net = emb_network
+
+        # The generator network
+        self.generator = Generator(
+            emb_dim = emb_dim,
+            proj_emb_dim = proj_emb_dim,
+            noise_dim = noise_dim,
+            num_gen_features = num_gen_features
+        )
+
+        # The discriminator network
         self.discriminator = Discriminator()
         
-        print("Creating GAN INT CLS Model")
-
 
     def fit(self, train_dataloader, val_dataloader = None, device = "cuda" if torch.cuda.is_available() else "cpu", epochs = 600):
-        
+        #TODO: Finish to implement the fit function
         
         for epoch in range(epochs):
             
@@ -62,5 +138,18 @@ class GAN_INT_CLS(nn.Module):
                 images = images.to(device)
                 captions = captions.to(device)
         
-                self.generator()
-        
+                self.generator(captions[0])
+    
+
+    def summary(self):
+        """ 
+        # Summary
+        Prints the summary of the model 
+        """
+
+        # Generator summary
+        summary(self.generator, [(1, self.emb_dim), (1, self.noise_dim)])
+
+        # TODO: Discriminator summary
+
+        # TODO: GAN-INT-CLS summary
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import clip
 
 from gan_t2i.models.GAN_INT_CLS import GAN_INT_CLS
 from gan_t2i.datasets.DatasetFactory import DatasetFactory
+from gan_t2i.models.CLIP import CLIPModel
+from gan_t2i.utils.model_loading import download_CLIP_model, CLIP_DATASETS
 
 import torch 
 from torch.utils.data import DataLoader, SubsetRandomSampler
@@ -12,7 +14,7 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 
-
+# Images transformations
 transform_img = transforms.Compose([
     transforms.Resize(224, interpolation=Image.BICUBIC),
     transforms.CenterCrop(224),
@@ -20,17 +22,19 @@ transform_img = transforms.Compose([
     transforms.Normalize([0.4355, 0.3777, 0.2879], [0.2571, 0.2028, 0.2101])
 ])
 
+# Text transformation (caption tokenizer)
 def tokenize_text(text):
     try:
         return clip.tokenize([text])[0]
     except:
         return clip.tokenize([text.split(".")[0]])[0] 
 
-    
+
+# Load data
 data_path = os.path.join(os.getcwd(), "data") 
 dataset = DatasetFactory.Flowers(data_path, transform_img=transform_img, transform_caption=tokenize_text)
 
-
+# Split data into train, validation and test
 t_size = int(0.85 * len(dataset))
 v_size = int(0.1 * len(dataset))
 t_size = len(dataset) - v_size - t_size
@@ -43,6 +47,15 @@ train_loader = DataLoader(dataset, batch_size=64, sampler=train_sampler, pin_mem
 val_loader = DataLoader(dataset, batch_size=64, sampler=val_sampler, pin_memory=True)
 test_loader = DataLoader(dataset, batch_size=64, sampler=test_sampler, pin_memory=True) 
 
+# Get the caption embedding network (our finetuned version of CLIP)
+checkpoint_path = download_CLIP_model(CLIP_DATASETS.FLOWERS)
+clip_emb_model = CLIPModel.load(checkpoint_path)
 
-gan = GAN_INT_CLS()
-gan.fit(train_loader, val_loader)
+# Create the GAN-INT-CLS model
+gan = GAN_INT_CLS(
+    emb_network=clip_emb_model,
+    emb_dim=512
+)
+gan.summary()
+
+# gan.fit(train_loader, val_loader)
